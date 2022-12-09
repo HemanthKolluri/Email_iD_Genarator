251019346c94f81c2ec6c7ef443f28b058cb52ce
HemanthKolluri <68379389+HemanthKolluri@users.noreply.github.com>
2022-12-09 19:20:11 +0530

    final file uploded

---
+++ Email_ID_Generator.py
@@ -0,0 +1,7 @@
+print("hello world")
+print("Enter your first name")
+F=input("")
+print("Enter your Last name")
+L=input("")
+N=len(F+L)
+print("your generated email id is "+F+L+str(N)+"@gmail.com")
